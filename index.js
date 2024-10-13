8aeb2eadd9e743ba99a9d3c25afbadf77cbbdc09
Ibnu Rofik <ibnurofik313@gmail.com>
2024-10-13 20:12:54 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+
+// app.use((req, res) => {
+//   console.log("we got request");
+//   res.send("ini string dari respon");
+// });
+
+// localhost:8080/cats - mengirim string
+
+app.get("/", (req, res) => {
+  res.send("This is homepage");
+});
+
+app.get("/cats", (req, res) => {
+  res.send("This is cats page");
+});
+
+app.get("/about", (req, res) => {
+  res.send("This is about page");
+});
+
+// app.get("/blog", (req, res) => {
+//   res.send("Ini halaman blog");
+// });
+
+app.get("blog/:judul", (req, res) => {
+  const judulBlog = req.params.judul;
+  console.log(judulBlog);
+  res.send("Anda sedang membaca artikel: " + judulBlog);
+});
+
+app.get("*", (req, res) => {
+  res.send("halaman tidak ditemukan");
+});
+
+app.listen(8080, () => {
+  console.log("Server is running on http://localhost:8080");
+});
